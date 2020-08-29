02d01317719f4e0b464bca917a4986eae982e32f
Oleksandr Volkov <fromobileprima@gmail.com>
2020-08-30 00:32:27 +0300

    fixed varivy by express-validator

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
+ANONTER WAY. Here we use formidable insted multer to save image in server.
+This way give us oportunity save image only if text params are verified, but we can't use express-validator well vecause express-validator  cant't wotk with any object, only some template. we should pass some object wich contains key "body", wich contains list of fields
+
+So we create custom req with field body wich contains our fields
+
 Reagister and login with mongoBD with opportunity add image in server and pass the path into the data base
 STACK:
     * express
     * React
     * mongoDb + mongoose
     * jsonwebtoken and bcrypt
-    * multer for upload image and save them on server 
+    * formidable for upload image and save them on server 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express")
-const { body, validationResult } = require('express-validator');
+const { body, check, validationResult } = require('express-validator');
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken')
@@ -60,48 +60,65 @@ router.post(
     (req, res) => {
         const form = formidable({ multiples: true });
 
-        form.parse(req, async(err, fields, files) => {
+        form.parse(req,
+            async(err, fields, files) => {
+                const customReq = { // express-validator can't work with custom fields, we should use template
+                    body: {
+                        ...fields
+                    }
+                };
 
-            const { email, userName, password } = fields;
+                await check('email', 'Invalid email').isEmail().run(customReq);
+                await check('userName', 'min length userName id 5 character').isLength({ min: 5 }).run(customReq);
+                await check('password', 'min length userName id 7 character').isLength({ min: 7 }).run(customReq);
 
-            const user = await User.findOne({ $or: [{ email: email }, { userName: userName }] });
+                const result = validationResult(customReq);
 
-            if (user) {
-                if (user.email === email) {
-                    return res.status(400).json({
-                        message: "user with such email already exist "
-                    })
-                } else if (user.userName === userName) {
-                    return res.status(400).json({
-                        message: "user with such userName already exist "
-                    })
+                if (!result.isEmpty()) {
+                    return res.status(400).json({ errors: result.array() });
                 }
-            }
 
-            const oldPath = files.avatar.path;
-            const newPath = "/home/senya/work/register-login/uploadImages/" + files.avatar.name
-            const rawData = await fs.readFileSync(oldPath)
 
-            await fs.writeFile(newPath, rawData, function(err) {
-                if (err) console.log(err)
-                console.log("Successfully uploaded")
+                const { email, userName, password } = fields;
+
+                const user = await User.findOne({ $or: [{ email: email }, { userName: userName }] });
+
+                if (user) {
+                    if (user.email === email) {
+                        return res.status(400).json({
+                            message: "user with such email already exist "
+                        })
+                    } else if (user.userName === userName) {
+                        return res.status(400).json({
+                            message: "user with such userName already exist "
+                        })
+                    }
+                }
+
+                const oldPath = files.avatar.path;
+                const newPath = "/home/senya/work/register-login/uploadImages/" + Date.now() + files.avatar.name
+                const rawData = await fs.readFileSync(oldPath)
+
+                await fs.writeFile(newPath, rawData, function(err) {
+                    if (err) console.log(err)
+                        // console.log("Successfully uploaded")
+                })
+
+
+                const hashedPassword = await bcrypt.hash(password, 12);
+
+                const newUser = new User({
+                    email,
+                    userName,
+                    password: hashedPassword,
+                    avatar: config.get('baseURL') + '/uploadImages/' + files.avatar.name
+                })
+
+                await newUser.save()
+
+                res.status(200).json({ user: newUser, message: "You was registered" })
+
             })
-
-
-            const hashedPassword = await bcrypt.hash(password, 12);
-
-            const newUser = new User({
-                email,
-                userName,
-                password: hashedPassword,
-                avatar: config.get('baseURL') + '/uploadImages/' + files.avatar.name
-            })
-
-            await newUser.save()
-
-            res.status(200).json({ user: newUser, message: "You was registered" })
-
-        })
     })
 
 
